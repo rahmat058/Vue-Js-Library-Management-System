cec1ecbb3877e90d4091a59726b303b397c47d09
Kazi Rahamatullah <kazirahmat58@gmail.com>
2018-03-04 09:13:47 +0600

    Library Management System Project StartUp

---
+++ client/main.js
@@ -0,0 +1,15 @@
+// Libs
+import {Meteor} from 'meteor/meteor';
+import {Vue} from 'meteor/akryum:vue';
+
+
+import '/imports/ui/theme/layout/layout.scss';
+// Main app
+import App from '/imports/ui/App.vue';
+
+
+Meteor.startup(() => {
+  new Vue({
+    render: h => h(App),
+  }).$mount('app');
+});
---
+++ imports/ui/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="rahmat">
+    <h2>Library Management System(lms)</h2>
+
+    <div class="row">
+      <div class=" red">
+
+      </div>
+      <div class=" green">
+
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
