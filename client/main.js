b7c3e70210729047cfb93191451e4b1be17ea412
Kazi Rahamatullah <kazirahmat58@gmail.com>
2018-03-10 02:28:20 +0600

    Login Page Design

--- client/main.js
+++ client/main.js
@@ -7,9 +7,15 @@ import '/imports/ui/theme/layout/layout.scss';
 // Main app
 import App from '/imports/ui/App.vue';
 
+//test App
+import Test from '/imports/ui/test.vue';
 
 Meteor.startup(() => {
   new Vue({
     render: h => h(App),
   }).$mount('app');
+
+  new Vue({
+    render: h => h(Test),
+  }).$mount('test');
 });
--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,17 +1,48 @@
 <template>
-  <div class="rahmat">
-    <h2>Library Management System(lms)</h2>
+  <div class="login">
 
-    <div class="row">
-      <div class=" red">
-
-      </div>
-      <div class=" green">
-
-      </div>
+    <div class="col-lg-12 text-center ">
+        <h1 style="font-family:Lucida Console">Library Management System</h1>
     </div>
-  </div>
 
+    <br>
+
+    <div class="login_wrapper">
+        <section class="login_content">
+            <form name="form1" action="" method="post">
+                <h1>User Login Form</h1>
+
+                <div>
+                    <input type="text" name="username" class="form-control" placeholder="Username" required=""/>
+                </div>
+                <div>
+                    <input type="password" name="password" class="form-control" placeholder="Password" required=""/>
+                </div>
+                <div>
+
+                    <input class="btn btn-default submit" type="submit" name="submit1" value="Login">
+                    <a class="reset_pass" href="#">Lost your password?</a>
+                </div>
+
+                <div class="clearfix"></div>
+
+                <div class="separator">
+                    <p class="change_link">New to site?
+                        <a href="registration.html"> Create Account </a>
+                    </p>
+
+                    <div class="clearfix"></div>
+                    <br/>
+                </div>
+            </form>
+        </section>
+    </div>
+
+    <div class="alert alert-danger col-lg-6 col-lg-push-3">
+        <strong style="color:white">Invalid</strong> Username Or Password.
+    </div>
+
+  </div>
 </template>
 
 <script>
