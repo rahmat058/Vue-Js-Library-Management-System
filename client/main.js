a16c6daa161587d7fd039782dd1394c3f4e8cec7
Kazi Rahamatullah <kazirahmat58@gmail.com>
2018-03-10 21:25:24 +0600

    Admin page design is working now

--- client/main.js
+++ client/main.js
@@ -13,6 +13,9 @@ import '/imports/ui/theme/layout/layout.scss';
 //Registration App
 import Registration from '/imports/ui/Registration.vue';
 
+//Admin App
+import Admin from '/imports/ui/Admin.vue';
+
 Meteor.startup(() => {
   // new Vue({
   //   render: h => h(App),
@@ -25,4 +28,8 @@ Meteor.startup(() => {
   new Vue({
     render: h => h(Registration),
   }).$mount('registration');
+
+  new Vue({
+    render: h => h(Admin),
+  }).$mount('admin');
 });
